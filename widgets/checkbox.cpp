4765eeaef20d6a2af07d4d2f1634e05bce9838fb
pedapudi <skpedapudi@gmail.com>
2026-01-16 19:39:48 -0800

    feat(core): expose Align enum and update widgets to use it (fixes #20)

--- widgets/checkbox.cpp
+++ widgets/checkbox.cpp
@@ -46,7 +46,7 @@ Checkbox& Checkbox::set_size(int32_t width, int32_t height) {
   Object::set_size(width, height);
   return *this;
 }
-Checkbox& Checkbox::align(lv_align_t align, int32_t x_ofs, int32_t y_ofs) {
+Checkbox& Checkbox::align(Object::Align align, int32_t x_ofs, int32_t y_ofs) {
   Object::align(align, x_ofs, y_ofs);
   return *this;
 }
